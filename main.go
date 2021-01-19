44e21d31b21b0d3a08c6160604cc2ec5f9cc778a
闫春浩 <601266867@qq.com>
2021-01-19 15:59:38 +0800

    666

--- main.go
+++ main.go
@@ -5,27 +5,21 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"yyy/controller"
 	_ "yyy/controller"
-	model "yyy/models"
+	"yyy/tool"
 )
 
 func init() {
-	//core.StsrtServer()
+
 }
 
 func main() {
-	//sec, err := config.Cfg.GetSection("database")
-	//if err != nil {
-	//	log.Fatal(2, "Fail to get section 'database': %v", err)
-	//}
-	//name := sec.Key("NAME")
-	//log.Println(name)
+	cfg, err := tool.ParseConfig("./config/app.json")
+	if err != nil {
+		panic(err.Error())
+	}
 
-	//db.Link()
-
-	model.Insert()
-
-	r := controller.R
-	if err := r.Run(); err != nil {
+	app := controller.R
+	if err := app.Run(cfg.AppHost + ":" + cfg.AppPort); err != nil {
 		fmt.Println("startup service failed, err:%v\n", err)
 	}
 }
--- models/baseModel.go
+++ models/baseModel.go
@@ -1,5 +1,35 @@
 package model
 
-import "yyy/core"
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
 
-var DB = core.Db
+//数据库配置
+const (
+	USERNAME = "root"
+	PASSWORD = "root"
+	NETWORK  = "tcp"
+	SERVER   = "127.0.0.1"
+	PORT     = 3306
+	DATABASE = "golang"
+)
+
+var Db *gorm.DB
+
+func init() {
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
+	var err error
+	Db, err = gorm.Open("mysql", dsn)
+
+	//Db.SingularTable(true)
+	//Db.LogMode(true)
+	//Db.DB().SetMaxIdleConns(10)
+	//Db.DB().SetMaxOpenConns(100)
+
+	if err != nil {
+		panic(err)
+	}
+
+	//defer Db.Close()
+}
